345e861bb04826e2b90724c3cf2fbb272ee2141c
Danilo <danilo.maida95@libero.it>
2020-03-26 12:27:28 +0100

    CSS- added

--- script/script.js
+++ script/script.js
@@ -11,19 +11,30 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.*/
 for (var i = 1; i <= 100; i++) {
   // creo una variabile a cui assegno il valore i
   var numero = i;
-
+  // variabile per stampare il contenuto precedente ad ogni ciclo
+  var content = document.getElementById('my_id').innerHTML;
+  // variabile per il console-log
+  var printLog;
   //condizione in cui stampare numeri o frase
 
   // se è multiplo di 3 o di 5
   if ( (numero % 3 == 0) && (numero % 5 == 0) ) {
-  numero = "FizzBuzz";
+    numero = "<li class='double'>" + "FizzBuzz" + "</li>";
+    printLog = "FizzBuzz";
   //se è multiplo di 3
   }else if (numero % 3 == 0) {
-    numero = "Fizz";
+    numero = "<li class='threeonly'>" + "Fizz" + "</li>";
+    printLog = "Fizz";
     //se è multiplo di 5
   }else if (numero % 5 == 0) {
-    numero = "Buzz";
+    numero = "<li class='fiveonly'>" + "Buzz" + "</li>";
+    printLog = "Buzz";
+  }else{
+      numero = "<li>" + i + "</li>";
+      printLog = i;
   }
   //stampo in console il valore che numero assume alla fine del ciclo
-  console.log(numero);
+  console.log(printLog);
+  // stampo a video La lista completa
+  document.getElementById('my_id').innerHTML = content +  numero;
 }
